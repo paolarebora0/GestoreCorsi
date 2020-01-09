d6663949d6c07532148fc37f3b6e87be236a7cae
Paola <Paola@192.168.1.104>
2020-01-09 10:14:11 +0100

    Finito punto 2.1

--- src/it/polito/tdp/corsi/GestoreCorsiController.java
+++ src/it/polito/tdp/corsi/GestoreCorsiController.java
@@ -2,10 +2,13 @@ package it.polito.tdp.corsi;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Map.Entry;
 
 import it.polito.tdp.corsi.model.Corso;
 import it.polito.tdp.corsi.model.GestoreCorsi;
+import it.polito.tdp.corsi.model.Studente;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -33,6 +36,15 @@ public class GestoreCorsiController {
 
     @FXML
     private Button btnStatCorsi;
+    
+    @FXML
+    private TextField txtCorso;
+
+    @FXML
+    private Button btnStudentiCorso;
+
+    @FXML
+    private Button btnCDS;
 
     @FXML
     void doCalcolaStatCorsi(ActionEvent event) {
@@ -40,15 +52,21 @@ public class GestoreCorsiController {
     	int periodo;
     	try {
     		periodo = Integer.parseInt(txtPeriodo.getText());
-		} catch (NumberFormatException e) {
-			txtResult.appendText("Devi inserire un periodo (1 o 2)");
+    	} catch (NumberFormatException e) {
+    		txtResult.appendText("Devi inserire un periodo (1 o 2)");
     		return;
-		}
+    	}
     	
     	if(periodo != 1 && periodo != 2) {
     		txtResult.appendText("Devi inserire un periodo(1 o 2)");
     		return;
     	}
+    	
+    	Map<Corso, Integer> res = model.getIscrittiCorsi(periodo);
+		
+		for(Entry entry: res.entrySet()) {
+			txtResult.appendText(((Corso)entry.getKey()).getNome()+ " = "+entry.getValue()+ "\n");
+		}
     	
     	
     }
@@ -75,12 +93,34 @@ public class GestoreCorsiController {
     	
     }
 
+    
+    @FXML
+    void doCalcolaStatCDS(ActionEvent event) {
+
+    	
+    }
+    
+    @FXML
+    void doElencaStudenti(ActionEvent event) {
+
+    	String codins = txtCorso.getText();
+    	
+    	List<Studente> studenti = this.model.elencaStudenti(codins);
+    	
+    	for(Studente s: studenti) {
+    		txtResult.appendText(s.toString() + "\n");
+    	}
+    	
+    }
     @FXML
     void initialize() {
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'GestoreCorsi.fxml'.";
         assert txtPeriodo != null : "fx:id=\"txtPeriodo\" was not injected: check your FXML file 'GestoreCorsi.fxml'.";
         assert btnCercaCorsi != null : "fx:id=\"btnCercaCorsi\" was not injected: check your FXML file 'GestoreCorsi.fxml'.";
         assert btnStatCorsi != null : "fx:id=\"btnStatCorsi\" was not injected: check your FXML file 'GestoreCorsi.fxml'.";
+        assert txtCorso != null : "fx:id=\"txtCorso\" was not injected: check your FXML file 'GestoreCorsi.fxml'.";
+        assert btnStudentiCorso != null : "fx:id=\"btnStudentiCorso\" was not injected: check your FXML file 'GestoreCorsi.fxml'.";
+        assert btnCDS != null : "fx:id=\"btnCDS\" was not injected: check your FXML file 'GestoreCorsi.fxml'.";
 
     }
     
--- src/it/polito/tdp/corsi/model/GestoreCorsi.java
+++ src/it/polito/tdp/corsi/model/GestoreCorsi.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import it.polito.tdp.corsi.db.CorsoDAO;
+import it.polito.tdp.corsi.db.StudenteDAO;
 import it.polito.tdp.corsi.model.Corso;
 
 //MODEL
@@ -43,8 +44,12 @@ public class GestoreCorsi {
 	public Map<Corso, Integer> getIscrittiCorsi(int periodo) {
 		
 		CorsoDAO dao = new CorsoDAO();
-		return dao.getIscrittiCorsi(periodo);
-		
-		
+		return dao.getIscrittiCorsi(periodo);	
+	}
+
+	
+	public List<Studente> elencaStudenti(String codins) {
+		StudenteDAO dao = new StudenteDAO();
+		return dao.elencaStudenti(codins);
 	}
 }
